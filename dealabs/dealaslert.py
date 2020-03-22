d9117fdd8a666887f30b06cd4c478f5d8d9b0d34
Gogo <glathugo@gmail.com>
2020-03-22 17:41:27 +0100

    Version V1.0

--- dealabs/dealaslert.py
+++ dealabs/dealaslert.py
@@ -1,82 +1,93 @@
-"""Platform for light integration."""
+"""Platform for sensor integration."""
 import logging
-import homeassistant
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+
+import requests
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
+from homeassistant.components.switch import PLATFORM_SCHEMA
+import xml.etree.ElementTree as ET
+from homeassistant.const import CONF_TOKEN
 
 _LOGGER = logging.getLogger(__name__)
+ATTR_TITLE: "Title"
+ATTR_CATEGORY: "Category"
+ATTR_MERCHANT: "Mercahnt"
+ATTR_PRICE: "Price"
+ATTR_DESCRIPTION: "Description"
+ATTR_LINK: "Link"
+ATTR_PUBLICATION_DAT: "Date of publication"
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36\''
+}
+
+URL = "https://www.dealabs.com/rssx/keyword-alarm/"
 
 # Validation of the user's configuration
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string,
-    vol.Optional(CONF_USERNAME, default='admin'): cv.string,
-    vol.Optional(CONF_PASSWORD): cv.string,
+    vol.Required(CONF_TOKEN): cv.string
 })
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Awesome Light platform."""
-    # Assign configuration variables.
-    # The configuration check takes care they are present.
-    host = config[CONF_HOST]
-    username = config[CONF_USERNAME]
-    password = config.get(CONF_PASSWORD)
-
-    # Setup connection with devices/cloud
-    hub = awesomelights.Hub(host, username, password)
-
-    # Verify that passed in configuration works
-    if not hub.is_valid_login():
-        _LOGGER.error("Could not connect to AwesomeLight hub")
-        return
-
-    # Add devices
-    add_entities(AwesomeLight(light) for light in hub.lights())
+    add_entities(DealabsSensor(config.get(CONF_TOKEN)))
 
 
-class AwesomeLight(Light):
-    """Representation of an Awesome Light."""
-
-    def __init__(self, light):
-        """Initialize an AwesomeLight."""
-        self._light = light
-        self._name = light.name
+class DealabsSensor(Entity):
+    """Representation of a Sensor."""
+    def __init__(self, token):
         self._state = None
-        self._brightness = None
+        self.category = None
+        self.merchant = None
+        self.price = None
+        self.title = None
+        self.description = None
+        self.link = None
+        self.pubDate = None
+        self.token = token
 
     @property
     def name(self):
-        """Return the display name of this light."""
-        return self._name
+        """Return the name of the sensor."""
+        return "Alertes Dealabas"
 
     @property
-    def brightness(self):
-        """Return the brightness of the light.
-        This method is optional. Removing it indicates to Home Assistant
-        that brightness is not supported for this light.
-        """
-        return self._brightness
-
-    @property
-    def is_on(self):
-        """Return true if light is on."""
+    def state(self):
+        """Return the state of the sensor."""
         return self._state
 
-    def turn_on(self, **kwargs):
-        """Instruct the light to turn on.
-        You can skip the brightness part if your light does not support
-        brightness control.
-        """
-        self._light.brightness = kwargs.get(ATTR_BRIGHTNESS, 255)
-        self._light.turn_on()
+    @property
+    def icon(self):
+        """Return the icon to use in the frontend."""
+        return "mdi:github-circle"
 
-    def turn_off(self, **kwargs):
-        """Instruct the light to turn off."""
-        self._light.turn_off()
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        attrs = {
+            ATTR_TITLE: self.title,
+            ATTR_CATEGORY: self.category,
+            ATTR_MERCHANT: self.merchant,
+            ATTR_PRICE: self.price,
+            ATTR_DESCRIPTION: self.description,
+            ATTR_LINK: self.link,
+            ATTR_PUBLICATION_DAT: self.pubDate
+        }
+        return attrs
 
     def update(self):
-        """Fetch new state data for this light.
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._light.update()
-        self._state = self._light.is_on()
-        self._brightness = self._light.brightness
+        join_url = URL + self.token;
+        rqt = requests.request(method='GET', url=join_url, headers=headers).text.encode('utf8')
+        tree = ET.fromstring(rqt)
+        nb = 0
+        for child in tree.findall('./channel/item'):
+            self.category = child.find('category').text
+            self.merchant = child.find('{http://www.pepper.com/rss}merchant').attrib.get('name')
+            self.price = child.find('{http://www.pepper.com/rss}merchant').attrib.get('price')
+            self.title = child.find('title').text
+            self.description = child.find('description').text
+            self.link = child.find('link').text
+            self.pubDate = child.find('pubDate').text
+            nb += 1
+        self._state = nb
